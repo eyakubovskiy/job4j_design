49887dfa0387d23efcdf5d39edb2457a0929202c
Evgeniy <eyakubovskiy@gmail.com>
2021-11-17 23:09:25 +0300

    checkstyle corrected 2

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -13,7 +13,7 @@ public class AppTest extends TestCase {
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName ) {
+    public AppTest(String testName) {
         super( testName );
     }
 
@@ -21,13 +21,13 @@ public class AppTest extends TestCase {
      * @return the suite of tests being tested
      */
     public static Test suite() {
-        return new TestSuite( AppTest.class );
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
     public void testApp() {
-        assertTrue( true );
+        assertTrue(true);
     }
 }
