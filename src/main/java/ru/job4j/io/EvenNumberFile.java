31bcc1491ef3d1918ad6348376a4560b6d7ff593
Evgeniy <eyakubovskiy@gmail.com>
2022-04-14 22:33:27 +0300

    0.2. FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        String template = "Текущее число %d является %s ";
+
+        try(FileInputStream file = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = file.read()) != -1) {
+                text.append((char) read);
+            }
+            String [] array = text.toString().split(System.lineSeparator());
+            for (String element : array) {
+                int valInt = Integer.parseInt(element);
+                String valString = (valInt % 2) == 0 ? "четным" : "нечетным";
+                String info = String.format(template, valInt, valString);
+                System.out.println(info);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
